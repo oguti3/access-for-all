521501e8ac4d3a6f6ab66de614407c239ec54417
GitUzer <zdarshan04@gmail.com>
2026-03-11 09:49:38 -0500

    walk signal timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshPro timerText;  // TextMeshPro for 3D object
+    [SerializeField] float remainingTime;
+    float elapsedTime;
+
+    void Update() {
+        
+        if (remainingTime > 0) {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0) {
+            remainingTime = 0;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60); 
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
