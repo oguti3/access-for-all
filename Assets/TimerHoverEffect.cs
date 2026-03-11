2f38b2a50b550bbb76f34795093bdfdc38bd1231
GitUzer <zdarshan04@gmail.com>
2026-03-11 11:07:40 -0500

    changing timer on walking sign

---
+++ Assets/TimerHoverEffect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TimerHoverEffect : MonoBehaviour
+{
+    [SerializeField] Color normalColor = Color.white;
+    [SerializeField] Color hoverColor = Color.yellow;
+    [SerializeField] float hoveredFontSize = 36f;
+
+    TextMeshPro timerText;
+    Timer timerScript;
+    bool isHovered = false;
+
+    void Start()
+    {
+        timerText = GetComponent<TextMeshPro>();
+        timerScript = GetComponent<Timer>();
+        timerText.color = normalColor;
+        timerText.alignment = TextAlignmentOptions.Center;
+    }
+
+    void OnWandPointing(CAVE2.WandEvent playerInfo)
+    {
+        isHovered = true;
+    }
+
+    void Update()
+    {
+        if (isHovered)
+        {
+            timerScript.enabled = false;
+            timerText.color = hoverColor;
+            timerText.text = "\u221E";
+            timerText.fontSize = hoveredFontSize;
+            timerText.alignment = TextAlignmentOptions.Center;
+        }
+    }
+}
