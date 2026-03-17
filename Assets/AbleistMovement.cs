b6589d769e346ac25daeee0d0a2a270b97a4036b
Esteban Sanz <estebanzcol@gmail.com>
2026-03-16 22:29:10 -0500

    more grocery scene

---
+++ Assets/AbleistMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbleistMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float moveDistance = 5f;
+
+    public MoveShelf1 shelfScript;
+
+    private bool shouldMove = false;
+    private bool limitReached = false;
+
+    void Start() {
+        shelfScript = FindObjectOfType<MoveShelf1>();
+    }
+
+
+    void Update()
+    {
+        if (shelfScript == null)
+        {
+            Debug.Log("shelfScript is NULL");
+            return;
+        }
+        if (shelfScript == null) return;
+
+        Debug.Log("ShelfPosition-Z: " + shelfScript.transform.position.z);
+
+        if (shelfScript.transform.position.z >= -0.04f)
+        {
+            shouldMove = true;
+        }
+
+        if (shouldMove && !limitReached)
+        {
+            Debug.Log("Reached");
+            transform.position += new Vector3(0f, 0f, 0.01f);
+            if (transform.position.z >= -2)
+            {
+                limitReached = true;
+            }
+
+        }
+
+        /*if (limitReached) {
+            
+        }*/
+
+    }    
+}
+
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AbleistMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float moveDistance = 5f; // how far right they go
+
+    public MoveShelf6 shelfScript;
+
+    private Vector3 shelfCurrPos;
+    private Vector3 startPos;
+
+    private bool shouldMove = false;
+    private bool limitReached = false;
+
+
+
+    private Vector3 shelfStartPos;
+
+    void Start()
+    {
+        shelfCurrPos = GameObject.FindWithTag("MoveableShelves").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ShelfPosition-Z: " + shelfCurrPos.z);
+        if (shelfScript.transform.position.z >= -1f)
+        {
+            shouldMove = true;
+        }
+
+
+        // move right after shelf moved
+        if (shouldMove && !limitReached)
+        {
+            Debug.Log("Reached!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            transform.position += new Vector3(0f, 0f, 3.56f);
+            limitReached = true;
+        }
+    }
+}*/
--- Assets/MoveShelf1.cs
+++ Assets/MoveShelf1.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class MoveShelf1 : CAVE2Interactable
 {
     int count = 0;
-    bool hasMoved = false;
-    bool isPointing = false; // back to manual flag
+    bool hasMoved, isPointing, startMovement = false; // back to manual flag
 
     WandPointer wandPointer;
 
@@ -27,18 +26,29 @@ public class MoveShelf1 : CAVE2Interactable
 
         if (wandPointer == null) return;
 
-        if (wandPointer.laserActivated && isPointing && !hasMoved)
+        if (wandPointer.laserActivated && isPointing)
         {
-            if (count < 120)
+            startMovement = true;
+        }
+
+        if (startMovement && !hasMoved)
+        {
+            if (count < 10)
             {
                 count++;
-                Debug.Log(gameObject.name + " count: " + count);
+               // Debug.Log(gameObject.name + " count: " + count);
             }
             else
             {
-                Debug.Log(gameObject.name + " MOVING NOW");
-                transform.position += new Vector3(0f, 0f, 3.56f);
-                hasMoved = true;
+                //Debug.Log(gameObject.name + " MOVING NOW");
+
+                if (transform.position.z >= 0f) 
+                {
+                    hasMoved = true;
+                }
+
+                transform.position += new Vector3(0f, 0f, 0.01f);
+                
             }
         }
         else
@@ -49,9 +59,8 @@ public class MoveShelf1 : CAVE2Interactable
         isPointing = false; // reset every frame
     }
 
-    public new void OnWandPointing(CAVE2.WandEvent playerInfo)
+    void OnWandPointing(CAVE2.WandEvent playerInfo)
     {
-        base.OnWandPointing(playerInfo);
         isPointing = true; // only true if ray hits shelf this frame
     }
 }
