b97b2175a8889d0346cf6627d47954ad6b1183d2
dorti8 <domar0405@gmail.com>
2026-03-31 08:38:31 -0500

    Added 2nd round of cars

---
+++ Assets/PlayerCarCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarCollision : MonoBehaviour
+
+{
+    public string targetTag;
+    public Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(targetTag))
+        {
+            this.transform.position = startingPosition;
+        }
+    }
+}
